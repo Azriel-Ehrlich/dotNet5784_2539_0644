a66df2583d4236e191a34e34f31a6d296ace4770
Azriel-Ehrlich <129371825+Azriel-Ehrlich@users.noreply.github.com>
2023-12-27 10:05:05 +0200

    added welcome func

---
+++ Stage0/Program2539.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2539();
+            Welcome0644();
+
+        }
+        private static partial void Welcome0644();
+        private static void Welcome2539()
+        {
+            Console.WriteLine("Enter your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0},Welcome to my first console application",userName);
+        }
+    }
+}
