5e797f85e8115b928106ab30e139db23ca93c3ee
Azriel-Ehrlich <129371825+Azriel-Ehrlich@users.noreply.github.com>
2024-01-09 20:47:35 +0200

    Revert "comments to the exceptions"
    This reverts commit cbf688c33f054f9bb96f61066cd9a15d5e7f722b.

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -1,24 +1,18 @@
 ﻿namespace DO;
 
-/// <summary>
-/// exception for when a dal doesn't exist for read, update or delete
-/// </summary>
+
 [Serializable]
 public class DalDoesNotExistException : Exception
 {
 	public DalDoesNotExistException(string? message) : base(message) { }
 }
-/// <summary>
-/// exception for when a dal already exists for create
-/// </summary>
+
 [Serializable]
 public class DalAlreadyExistsException : Exception
 {
 	public DalAlreadyExistsException(string? message) : base(message) { }
 }
-/// <summary>
-/// exception for when a dal has invalid input for create or update
-/// </summary>
+
 [Serializable]
 public class DalInvalidInputException : Exception
 {
