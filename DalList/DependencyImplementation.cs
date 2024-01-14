53735b5ee00e5bbc0fd32bb6b62a3bd11d353e38
ariel halili <arielhalili259@gmail.com>
2024-01-14 11:55:04 +0200

    start stage 3: the amzing XML :D

--- DalList/DependencyImplementation.cs
+++ DalList/DependencyImplementation.cs
@@ -9,14 +9,14 @@ internal class DependencyImplementation : IDependency
 	public int Create(Dependency item)
 	{
 		Dependency newDep = item with { Id = DataSource.Config.NextDependencyId };
-		DataSource.Dependencys.Add(newDep);
+		DataSource.Dependencies.Add(newDep);
 		return newDep.Id;
 	}
 
 	public void Delete(int id)
 	{
 		Dependency? dep = Read(id) ?? throw new DalDoesNotExistException($"Dependency with id {id} doesn't exist");
-		DataSource.Dependencys.Remove(dep);
+		DataSource.Dependencies.Remove(dep);
 	}
 
 	public Dependency? Read(int id)
@@ -26,20 +26,20 @@ internal class DependencyImplementation : IDependency
 
 	public Dependency? Read(Func<Dependency, bool> filter)
 	{
-		return DataSource.Dependencys.Where(filter).FirstOrDefault();
+		return DataSource.Dependencies.Where(filter).FirstOrDefault();
 	}
 
 	public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
 	{
 		if (filter is null)
-			return DataSource.Dependencys.Select(e => e);
-		return DataSource.Dependencys.Where(filter);
+			return DataSource.Dependencies.Select(e => e);
+		return DataSource.Dependencies.Where(filter);
 	}
 
 	public void Update(Dependency item)
 	{
 		Dependency? dep = Read(item.Id) ?? throw new DalDoesNotExistException($"Dependency with id {item.Id} doesn't exist");
-		DataSource.Dependencys.Remove(dep); // remove the old item
-		DataSource.Dependencys.Add(item); // and add the new one
+		DataSource.Dependencies.Remove(dep); // remove the old item
+		DataSource.Dependencies.Add(item); // and add the new one
 	}
 }
