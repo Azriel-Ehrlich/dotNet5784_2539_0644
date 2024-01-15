51079680797d68c5a8a6912f1904e2af27918e35
Azriel-Ehrlich <129371825+Azriel-Ehrlich@users.noreply.github.com>
2024-01-15 13:41:01 +0200

    task xml implemented

--- DalXml/TaskImplementation.cs
+++ DalXml/TaskImplementation.cs
@@ -10,31 +10,46 @@ internal class TaskImplementation : ITask
 
 	public int Create(Task item)
 	{
-		throw new NotImplementedException();
+		List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+		if (tasks.Where(t => t.Id == item.Id).FirstOrDefault() is not null)
+            throw new DalAlreadyExistsException($"Task with id {item.Id} already exists");
+		int id = Config.NextTaskId;
+		tasks.Add(item with { Id = id });
+		XMLTools.SaveListToXMLSerializer(tasks, s_tasks_xml);
+		return id;
 	}
 
 	public void Delete(int id)
 	{
-		throw new NotImplementedException();
+		List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+        Task? task = tasks.Where(t => t.Id == id).FirstOrDefault() ?? throw new DalDoesNotExistException($"Task with id {id} doesn't exist");
+		Update(task with { Active = false });
 	}
 
 	public Task? Read(int id)
 	{
-		throw new NotImplementedException();
+		return Read(x => x.Id == id);
 	}
 
 	public Task? Read(Func<Task, bool> filter)
 	{
-		throw new NotImplementedException();
+		return ReadAll(filter).FirstOrDefault();
 	}
 
 	public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
 	{
-		throw new NotImplementedException();
+		List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+		if (filter is null)
+            return tasks.Select(t => t);
+		return tasks.Where(filter);
 	}
 
 	public void Update(Task item)
 	{
-		throw new NotImplementedException();
+		List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+        Task? task = Read(item.Id) ?? throw new DalDoesNotExistException($"Task with id {item.Id} doesn't exist");
+		tasks.Remove(task);
+		tasks.Add(item);
+		XMLTools.SaveListToXMLSerializer(tasks, s_tasks_xml);
 	}
 }
