209e8c820b1ad6b9a82384bc8123ae7435ead076
Azriel-Ehrlich <129371825+Azriel-Ehrlich@users.noreply.github.com>
2024-02-25 22:40:14 +0200

    done with scheduled dates

--- PL/Manager/SuggestedDate.xaml.cs
+++ PL/Manager/SuggestedDate.xaml.cs
@@ -19,9 +19,50 @@ namespace PL.Manager
     /// </summary>
     public partial class SuggestedDate : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+
+        public DateTime StartProj
+        {
+            get { return (DateTime)GetValue(StartProjProperty); }
+            set { SetValue(StartProjProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StartProj.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StartProjProperty =
+            DependencyProperty.Register("StartProj", typeof(DateTime), typeof(SuggestedDate), new PropertyMetadata(null));
+
+
+
         public SuggestedDate()
         {
             InitializeComponent();
+        }
+
+        private void ScheudledDate_Button(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                foreach (var t in s_bl.Task.ReadAll(t => t.Dependencies is null))
+                {
+                    s_bl.Task.UpdateScheduledDate(t.Id, StartProj);
+
+                }
+                foreach (var t in s_bl.Task.ReadAll(t => t.Dependencies is not null))
+                {
+                    DateTime? date = s_bl.SuggestedDate(t, StartProj);
+                    if (date is not null)
+                    {
+                        s_bl.Task.UpdateScheduledDate(t.Id, date.Value);
+                    }
+                }
+                s_bl.SaveScheduledDate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            this.Close();
         }
     }
 }
